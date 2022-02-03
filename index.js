95f02c2913f5d12f67304bc8c09d38f96fe20aa4
ericdwkim <ericdwkim.io@gmail.com>
2022-02-03 14:28:45 -0500

    left off at goodbye-cdns module

--- index.js
+++ index.js
@@ -60,3 +60,76 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 )
+
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+
+// ReactDOM.render(
+    
+//     <h1 className="header">
+//     "Hello World"
+//     </h1>,
+//     document.getElementById("root")
+        
+        
+//     )// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+
+const navbar = (
+    <nav>
+            <h1> Bob's Bistro</h1>
+            <ul>
+                    <li> Pricing</li>
+                    <li> About</li>
+                    <li> Contact</li>
+            </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
